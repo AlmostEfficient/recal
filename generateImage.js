6a3b6817750d706f71f3a5c271bfc076d027c684
Abdullah Raza <42661870+AlmostEfficient@users.noreply.github.com>
2023-09-22 18:19:42 +1200

    fully working, v0.1 complete

--- generateImage.js
+++ generateImage.js
@@ -1,28 +1,84 @@
 import fs from 'fs';
 import path from 'path';
 import puppeteer from 'puppeteer';
+import { google } from 'googleapis';
+import { format, isToday, parseISO, isWithinInterval, addDays } from 'date-fns';
 
-// Delete the old screenshot if it exists
-if (fs.existsSync('suspended.png')) {
-    fs.unlinkSync('suspended.png');
-}
+// Setup the API client
+const calendarId = 'ENTER_YOUR_CALENDAR_ID_HERE';
+const keyFile = 'credentials.json';
+const keys = JSON.parse(fs.readFileSync(keyFile, 'utf8'));
+const auth = new google.auth.JWT(keys.client_email, null, keys.private_key, [
+  'https://www.googleapis.com/auth/calendar.readonly',
+]);
+
+const quotes = JSON.parse(fs.readFileSync('quotes.json', 'utf8'));
+const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+const calendar = google.calendar({ version: 'v3', auth });
 
 (async () => {
-  
-    const htmlContent = fs.readFileSync(path.resolve('preview.html'), 'utf-8');
-    
-    const browser = await puppeteer.launch({headless: 'new'});
-    const page = await browser.newPage();
-    
-    // Can adjust this for different devices later (phone, tablet, etc.)
-    await page.setViewport({
-        width: 1404,
-        height: 1872,
-    });
+  const now = new Date();
+  const threeDaysFromNow = addDays(now, 3);
 
-    await page.setContent(htmlContent);
-    
-    await page.screenshot({ path: 'suspended.png' });
-    
-    await browser.close();
+  // Fetching the events
+  const {
+    data: { items: events },
+  } = await calendar.events.list({
+    calendarId,
+    timeMin: now.toISOString(),
+    timeMax: threeDaysFromNow.toISOString(),
+    singleEvents: true,
+    orderBy: 'startTime',
+  });
+
+  // If no events found, exit
+  if (!events.length) {
+    console.log('No events found');
+    process.exit();
+  }
+  
+  // Populating the HTML content
+  let todayEvents = '';
+  let upcomingEvents = '';
+
+  events.forEach((event) => {
+    const start = parseISO(event.start.dateTime || event.start.date);
+    const formattedTime = format(start, 'hh:mm a');
+    const formattedDay = format(start, 'EEEE');
+
+    const eventHtml = `
+            <div class="event">
+                ${event.summary}
+                <div class="event-time">${formattedTime}</div>
+            </div>`;
+
+    const upcomingEventHtml = `<div class="upcoming-event">${formattedDay} - ${formattedTime}: ${event.summary}</div>`;
+
+    if (isToday(start)) todayEvents += eventHtml;
+    else upcomingEvents += upcomingEventHtml;
+  });
+
+  // Reading and Updating the HTML file
+  let htmlContent = fs.readFileSync(path.resolve('preview.html'), 'utf-8');
+  htmlContent = htmlContent.replace(
+    /<div class="events">[\s\S]*?<\/div>/,
+    `<div class="events">${todayEvents}</div>`
+  );
+  htmlContent = htmlContent.replace(
+    /<div class="upcoming-events">[\s\S]*?<\/div>/,
+    `<div class="upcoming-events">${upcomingEvents}</div>`
+  );
+  htmlContent = htmlContent.replace(
+    /<div class="quote">[\s\S]*?<\/div>/,
+    `<div class="quote">"${randomQuote.quote}"<br><span class="author">- ${randomQuote.author}</span></div>`
+  );
+
+  const browser = await puppeteer.launch({ headless: 'new' });
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1404, height: 1872 });
+  await page.setContent(htmlContent);
+  await page.screenshot({ path: 'suspended.png' });
+  await browser.close();
+  console.log('Image generated successfully');
 })();
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-This repo will fetch events from my iCloud and Google calendars. 
+This repo will fetch events from Google calendar and generate an image you can use as the suspended/sleep screen for your Remarkable 2. 
 
 Remarkable image dimensions
 1404x1872 72 dpi 8 bit PNG
@@ -7,13 +7,22 @@ V0.1
 Google Calendar
 
 V0.2
-iCloud
-
-# MVP 0.5
-1. Generate an image with Puppeteer. 
-2. Update the image to be 1404x1872 pixels and 72 DPI.
-3. Add date/time to the image.
+iCloud (soom `tm`)
 
 ## How this works
-1. `preview.html` is the HTML that's used to generate the content of the image.
-2. `generateImage.js` uses Puppeteer to load preview and generate the image.
+1. `preview.html` is the HTML that's used to generate the content of the image. Update styling/layout here.
+2. `generateImage.js` fetches events from the Google Calendar API and uses Puppeteer to load preview and generate the image.
+
+## Prerequisites
+1. Node.js installed
+2. Service account credentials for Google Calendar API. Follow [this guide](https://stateful.com/blog/events-in-the-google-calendar-API) to create a service account, download the credentials file, and share your calendar with the service account email address. You can stop following the guide when it gets to the Node.js setup.
+   <!-- 2. Go to the [Google Console](https://console.cloud.google.com/apis/dashboard) and create a new project (or use an existing one).
+   3. Enable the Google Calendar API in the [API library](https://console.cloud.google.com/apis/library/calendar-json.googleapis.com).
+   4. Go to [IAM & Admin](https://console.cloud.google.com/iam-admin/serviceaccounts) and create a new service account. 
+   5. Click the email that was just generated, navigate to the keys menu and add a new JSON key.
+   6.  -->
+## Setup steps
+1. npm i 
+2. Put your service account credentials in the root of the project and name it `credentials.json`.
+3. Update `ENTER_YOUR_CALENDAR_ID_HERE` on line 8 in `generateImage.js` with your calendar ID.
+4. Run `npm start`. A `suspended.png` will be generated.
